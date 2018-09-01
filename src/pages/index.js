ff2b8f160f171b79e18ec54ddd04b56dc1d71d91
messay f <info@messay.com>
2018-09-01 09:25:05 -0500

    Name styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+
 const IndexPage = () => (
   <div>
   <div className="hero">
   <div className="hero-group"> 
-    <h1>Messay F. Denbel</h1>
+  
+   <div className="name">
+   <div className="nameGroup"> 
+   <Link to="/"> <img src={require ('../imges/Logo.svg')} width="100"/> </Link>
+   <h1>Messay F. Denbel</h1> 
+   </div>
+    </div>
+    
     <p>HI, I am a product designer focused on creating digital experiences who has a passion for motion, visual design, human-centered designer. I really enjoy digging into complex user experience problems with a deliberate curious mindset. 
 </p>
     <p>Now go bucvxvild something great.</p>
