4d55f60427e51504567f37ead0fae9ee92f205d1
messay f <info@messay.com>
2018-08-31 22:43:36 -0500

    test commit

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'messay portfolio',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>HI my name is Messay Denbel</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">Go to page 2 save it</Link>
+  </div>
+)
+
+export default IndexPage
