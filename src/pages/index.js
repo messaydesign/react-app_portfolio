011266e106dceac21b9b757c4160003d4210aa31
messay <info@messay.design>
2018-09-04 16:52:37 -0500

    emoji added

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,10 +10,10 @@ const IndexPage = () => (
    <div className="name">
    <div className="nameGroup"> 
    <Link to="/"> <img src={require ('../imges/Logo.svg')} width="75"/> </Link>
-   <h1>Messay F. Denbel</h1> 
+   <h1>Messay F. Denbel  </h1> 
    </div>
     </div>
-    <div className="nameCont"> <p>HI, I am a product designer focused on creating digital experiences who has a passion for motion, visual design, human-centered designer. I really enjoy digging into complex user experience problems with a deliberate curious mindset. All of my work is based on applying the design process to a variety of problems. 
+    <div className="nameCont"> <p> <strong>  👋🏽  </strong>HI, I am a product designer focused on creating digital experiences who has a passion for motion, visual design, human-centered designer. I really enjoy digging into complex user experience problems with a deliberate curious mindset. All of my work is based on applying the design process to a variety of problems. 
       </p> 
     </div>
      <p></p>
@@ -37,7 +37,7 @@ const IndexPage = () => (
 
    <div className="mainGroup">
   <h1 calssName="main">
-  Let’s Build <br/> Something great together
+  Let’s Build <br/> Something great together 👍🏽
   </h1>
   <p>
   It is all about solving problems, if not it would be pixel art. Product design always starts with understanding the problem rigorously.The fun part being a product designer is that I have problems to solve every single day with collaborative problem-solving process. In my 7+ years journey I realize that close collaboration of multidisciplinary team with a design process creates a winning product. 
