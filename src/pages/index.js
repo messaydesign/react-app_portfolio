6e2baa690170e2cd683fd6e1e3f7dc7ef202fcf6
messay <info@messay.design>
2018-09-07 17:36:46 -0500

    git test

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ const IndexPage = () => (
    <h1>  <img src={require ('../imges/Logo.svg')} width="75"/> Messay F. Denbel  </h1> 
    </div>
     </div>
-    <div className="nameCont"> <p> <strong>👋🏽 Hi, </strong> <br/>I am a product designer focused on creating digital experiences who has a passion for motion, 
+    <div className="nameCont"> <p> <strong>👋🏽 Hi, whats up  </strong> <br/>I am a product designer focused on creating digital experiences who has a passion for motion, 
     visual design, human-centered designer. I really enjoy digging into complex user experience problems with a deliberate curious mindset. 
     All of my work is based on applying the design process to a variety of problems. 
       </p> 
